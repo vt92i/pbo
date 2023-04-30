ca9be2f76ad58d2a72b38f99176967f3dfa9c2e4
fikaa <benidiktusvio@gmail.com>
2023-04-30 20:45:05 +0700

    🔥 chore(misc.xml): remove SpellCheckerSettings component ✨ feat(Main.java): add basic Java Swing window The SpellCheckerSettings component was removed from the misc.xml file as it was not being used. A basic Java Swing window was added to the Main.java file, which displays a window with the title "Java Swing" and dimensions of 300x300 pixels. The window is not resizable and will close when the user clicks the close button.

--- IdeaProjects/java-swing/src/Main.java
+++ IdeaProjects/java-swing/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    JFrame frame = new JFrame();
+    frame.setTitle("Java Swing");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 300);
+    frame.setResizable(false);
+    frame.setVisible(true);
   }
 }
